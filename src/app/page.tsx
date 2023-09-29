0b85a965455eec3f36e9fcdec5ffba5564fdeddf
Animesh-03 <anivad03@gmail.com>
2023-09-29 18:17:06 +0530

    add leaflet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,10 @@ import { MdAtm } from 'react-icons/md';
 import { TbHotelService } from 'react-icons/tb';
 import { HiOutlineCamera } from 'react-icons/hi';
 
+import { MapContainer } from 'react-leaflet'
+import { TileLayer } from 'react-leaflet'
+import { useMap, Popup, Marker } from 'react-leaflet'
+
 import LeftPane from './leftpane';
 
 
@@ -20,7 +24,18 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-12">
-      <button
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+      {/* <button
         className="bg-blue-500 text-white p-2 rounded-full absolute top-4 left-2"
         onClick={toggleLeftPane}
       >
@@ -53,7 +68,7 @@ export default function Home() {
         </button>
 
       </div>
-      {isLeftPaneOpen && <LeftPane />}
+      {isLeftPaneOpen && <LeftPane />} */}
     </main>
   );
 }

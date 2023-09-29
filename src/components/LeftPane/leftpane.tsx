dde3fc2d2e847db654f5c967f7844017089ac5bf
Animesh-03 <anivad03@gmail.com>
2023-09-29 20:17:42 +0530

    fix animation

--- src/components/LeftPane/leftpane.tsx
+++ src/components/LeftPane/leftpane.tsx
@@ -11,11 +11,11 @@ export const LeftPane : React.FC<LeftPaneProps> = ({setIsLeftPaneActive}) => {
   const [isOpen, setIsOpen] = useState(true);
   const togglePane = () => {
     setIsOpen(!isOpen);
-    setTimeout(() => setIsLeftPaneActive(false), 1 * 1000)
+    setTimeout(() => setIsLeftPaneActive(false), 0.4 * 1000)
   };
   return (
     <div
-      className={`fixed left-0 top-0 h-full w-72 bg-white -translate-x-full transform transition-transform ease-in-out duration-300 ${isOpen ? 'translate-x-0' : '-translate-x-full' // Changed from -translate-x-64 to -translate-x-full
+      className={`text-black fixed left-0 top-0 h-full w-72 bg-white transform transition-transform ease-in-out duration-300 ${isOpen ? 'translate-x-0' : '-translate-x-full' // Changed from -translate-x-64 to -translate-x-full
         }`}
     >
       <button
